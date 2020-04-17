be2eef4ca7dad7501baa5ec36a4d14de8f58f611
aburk3 <aburk3@gmail.com>
2020-04-16 19:04:22 -0500

    Connected bootstrap to index.html file and made the homepage root.

--- entries/urls.py
+++ entries/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import HomeView
 
 urlpatterns = [
-    path('home/', HomeView.as_view(), name='blog-home')
+    path('', HomeView.as_view(), name='blog-home')
 ]
