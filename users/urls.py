1ff05a2eb980c33d484e47eb37fc3395a517fb98
ABurke <aburke@flexera.com>
2020-04-21 11:14:44 -0500

    Added code that allows a user to logout and be redirected to the login page.

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     
     # Django would automatically look for this view in a registration dir,
     # but since we don't have that, we specy where with 'template_name='
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
