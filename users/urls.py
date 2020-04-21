bf72c42975fe10103f65bc7159a43c8d238e0300
ABurke <aburke@flexera.com>
2020-04-21 11:09:29 -0500

    Added login page for user.

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name = 'register'),
+    
+    # Django would automatically look for this view in a registration dir,
+    # but since we don't have that, we specy where with 'template_name='
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login')
 ]
